6f87e671c745bb2612df2b5058afdf9e96c609db
camposvinicius <viniciuscampos199519@gmail.com>
2021-06-28 15:34:46 -0300

    atualizacao

--- infrastructure/glue_crawler.tf
+++ infrastructure/glue_crawler.tf
@@ -2,10 +2,10 @@ resource "aws_glue_catalog_database" "raw_vini" {
   name = "onboarding-a3-"
 }
 
-resource "aws_glue_crawler" "raw" {
+resource "aws_glue_crawler" "raw_vini" {
   database_name = aws_glue_catalog_database.raw_vini.name
   name          = "consumer-zone"
-  role          = aws_iam_role.glue_role.arn
+  role          = aws_iam_role.glue_role_vini.arn
 
   s3_target {
     path = "s3://${aws_s3_bucket.dl.bucket}/raw-data"
